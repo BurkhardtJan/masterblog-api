26ad20d6006412566aa30220ef8e05a65ed8c0ff
Jan Burkhardt <burkhardt.jan.august@gmail.com>
2026-04-20 20:59:12 +0200

    Added deletebroutine

--- backend/backend_app.py
+++ backend/backend_app.py
@@ -1,5 +1,6 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
+from blog_post_handler import BlogPost
 
 app = Flask(__name__)
 CORS(app)  # This will enable CORS for all routes
@@ -8,11 +9,49 @@ POSTS = [
     {"id": 1, "title": "First post", "content": "This is the first post."},
     {"id": 2, "title": "Second post", "content": "This is the second post."},
 ]
+blog_posts = BlogPost("posts.json")
 
 
 @app.route('/api/posts', methods=['GET'])
 def get_posts():
-    return jsonify(POSTS)
+    """Returns blog posts as json"""
+    posts_data = blog_posts.posts
+    return jsonify(posts_data)
+
+
+@app.route('/api/posts', methods=['POST'])
+def add_post():
+    """Adds a new post to the database."""
+    json_data = request.get_json()
+    if not "title" in json_data and not "content" in json_data:
+        return jsonify({"error": "title and content are required"}), 400
+    elif not "title" in json_data:
+        return jsonify({"error": "title is required"}), 400
+    elif not "content" in json_data:
+        return jsonify({"error": "content is required"}), 400
+    title = json_data["title"]
+    content = json_data["content"]
+    if title == "" and content == "":
+        return jsonify({"error": "title and content are required"}), 400
+    elif title == "":
+        return jsonify({"error": "title is required"}), 400
+    elif content == "":
+        return jsonify({"error": "content is required"}), 400
+    new_post = blog_posts.add(title, content)
+    return jsonify(new_post), 201
+
+
+@app.route('/api/posts/<int:post_id>', methods=['DELETE'])
+def delete(post_id):
+    """Delete a blog post by ID"""
+    post_to_delete = blog_posts.fetch_post_by_id(post_id)
+    if not post_to_delete:
+        return jsonify({"error": f"post with id {post_id} not found"}), 404
+    blog_posts.delete(post_id)
+    message = {
+        "message": f"Post with id {post_id} has been deleted successfully."
+    }
+    return jsonify(message), 200
 
 
 if __name__ == '__main__':
---
+++ backend/blog_post_handler.py
@@ -0,0 +1,73 @@
+import json
+import os
+
+
+class BlogPost:
+    """Manage blog posts in JSON."""
+
+    def __init__(self, filename):
+        self.filename = filename
+        self._posts = self._load_data()
+
+    def _load_data(self):
+        """Reads data from file or returns empty"""
+        if not os.path.exists(self.filename):
+            return []
+        with open(self.filename, 'r', encoding='utf-8') as f:
+            return json.load(f)
+
+    def _save_data(self):
+        """Saves data into in JSON."""
+        with open(self.filename, 'w', encoding='utf-8') as f:
+            json.dump(self._posts, f, indent=4)
+
+    @property
+    def posts(self):
+        """Returns list of blog posts."""
+        return self._posts
+
+    def add(self, title, content):
+        """Adds and saves blog post. Sets id to max(id)+1"""
+        new_id = max([p['id'] for p in self._posts], default=0) + 1
+
+        new_post = {
+            "id": new_id,
+            "title": title,
+            "content": content,
+        }
+        self._posts.append(new_post)
+        self._save_data()
+        return new_post
+
+    def delete(self, post_id):
+        """Deletes post with selected number."""
+        self._posts = [p for p in self._posts if p['id'] != post_id]
+        self._save_data()
+
+    def fetch_post_by_id(self, post_id):
+        """Fetches blog post with selected id."""
+        for post in self._posts:
+            if post['id'] == post_id:
+                return post
+        return None
+
+    def fetch_post_position_by_id(self, post_id):
+        """Fetches blog post position with selected id."""
+        i = 0
+        for post in self._posts:
+            if post['id'] == post_id:
+                return i
+            i += 1
+        return None
+
+    def change(self, post_id, title, content):
+        """Changes blog post with selected id."""
+        changed_post = {
+            "id": post_id,
+            "title": title,
+            "content": content,
+        }
+        post_index = self.fetch_post_position_by_id(post_id)
+        self._posts[post_index] = changed_post
+        self._save_data()
+
